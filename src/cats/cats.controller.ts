f19eb2749521ee0bb8dadc427854f7cedeccd476
Carlos Moran <cmoran@applaudostudios.com>
2021-07-24 00:28:56 -0600

    Controllers: resources

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Get, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
   @Get()
-  findAll(@Req() request: Request): string {
+  findAll(): string {
     return 'This action returns all cats';
   }
 }
+
+/**
+ * Nest provides decorator for all of the standard HTTP methods: @Get(),
+ * @Post(), @Put(), @Delete(), @Patch(), @Options() and @Head(). In addition
+ * @All() defines an endpoint that handles all of them
+ */
