1f142ae3203f35cddfbd6b4813eaac7d48c2f37d
Carlos Moran <cmoran@applaudostudios.com>
2021-07-24 00:35:57 -0600

    Controllers status code

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -4,6 +4,7 @@ import { Controller, Get, Post, HttpCode } from '@nestjs/common';
 @Controller('cats')
 export class CatsController {
   @Post()
+  @HttpCode(204)
   create(): string {
     return 'This action adds a new cat';
   }
@@ -18,4 +19,9 @@ export class CatsController {
  * Nest provides decorator for all of the standard HTTP methods: @Get(),
  * @Post(), @Put(), @Delete(), @Patch(), @Options() and @Head(). In addition
  * @All() defines an endpoint that handles all of them
+ */
+
+/**
+ * Often your status code isn't static but depends on various factors. In that case, you can use
+ * a library specific response (inject using @Res()) object (or, in case of an error, throw an exception).
  */
